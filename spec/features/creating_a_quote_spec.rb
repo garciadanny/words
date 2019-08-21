0a5fab39021eb402e2d6f14879a63e4fc99360a7
Danny Garcia <dannygarcia.me@gmail.com>
2019-08-21 12:38:41 -0400

    Remove layers of abstraction from feature specs
    Attempts to utilize the Compose Method pattern to remove layers of
    abstraction from feature specs. Before this change, user-level setup was
    commingled with page specific interaction logic; reducing context
    switching and telling a story that is easier to follow.
    
    See:
    https://thoughtbot.com/blog/acceptance-tests-at-a-single-level-of-abstraction
    
    See:
    http://c2.com/ppr/wiki/WikiPagesAboutRefactoring/ComposedMethod.html

--- spec/features/creating_a_quote_spec.rb
+++ spec/features/creating_a_quote_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 feature "Creating a new quote" do
   scenario "Creating a quote with included text" do
     visit new_quote_path
-    fill_in("quote_text", with: "Some quote text...")
-    fill_in("quote_source", with: "The internet")
-    fill_in("quote_author", with: "Some VC Guru")
 
-    click_button("Create")
+    create_quote(
+      text: "Some quote text...",
+      source: "The internet",
+      author: "Some VC Guru"
+    )
 
     expect(page).to have_current_path(quote_path(Quote.last))
     expect(page).to have_content("Some quote text")
@@ -17,14 +18,26 @@ feature "Creating a new quote" do
 
   scenario "Creating a quote without any text" do
     visit new_quote_path
-    fill_in("quote_source", with: "The internet")
-    fill_in("quote_author", with: "Some VC Guru")
 
-    click_button("Create")
+    create_quote(
+      source: "The internet",
+      author: "Some VC Guru"
+    )
 
     expect(page).to have_content("New Quote")
-    expect(find_field("quote_source").value).to eq("The internet")
-    expect(find_field("quote_author").value).to eq("Some VC Guru")
     expect(page).to have_content("Text can't be blank")
+    expect(value_of(field: "quote_source")).to eq("The internet")
+    expect(value_of(field: "quote_author")).to eq("Some VC Guru")
+  end
+
+  def create_quote(text: nil, source: nil, author: nil)
+    fill_in("quote_text", with: text)
+    fill_in("quote_source", with: source)
+    fill_in("quote_author", with: author)
+    click_button("Create")
+  end
+
+  def value_of(field:)
+    find_field(field).value
   end
 end
--- spec/features/viewing_the_quotes_home_page_spec.rb
+++ spec/features/viewing_the_quotes_home_page_spec.rb
@@ -4,30 +4,38 @@ feature "Views the quotes home page" do
   context "Given quotes are paginated to 10 per page" do
     context "Given 15 total quotes" do
       scenario "When viewing the first page" do
-        15.times do |n|
-          create_quote(text: "inspired#{n}!", source: "http://example.com")
-        end
+        create_quotes(count: 15)
+
         visit quotes_path
 
         expect(page).to have_content("Next Page")
         expect(page).not_to have_content("Previous Page")
-        expect(page).to have_selector("div.quote-summary", count: 10)
+        expect(page).to have_number_of_quotes(10)
       end
 
       scenario "When viewing the last page" do
-        15.times do |n|
-          create_quote(text: "inspired#{n}!", source: "http://example.com")
-        end
+        create_quotes(count: 15)
+
         visit quotes_path(page: 2)
 
         expect(page).not_to have_content("Next Page")
         expect(page).to have_content("Previous Page")
-        expect(page).to have_selector("div.quote-summary", count: 5)
+        expect(page).to have_number_of_quotes(5)
       end
+    end
+  end
+
+  def create_quotes(count:)
+    count.times do |n|
+      create_quote(text: "inspired#{n}!")
     end
   end
 
   def create_quote(text:, source: nil, author: nil)
     Quote.create(text: text, source: source, author: author)
+  end
+
+  def have_number_of_quotes(count)
+    have_selector("div.quote-summary", count: count)
   end
 end
